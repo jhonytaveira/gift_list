4381eea8fdce5540a42ba38181e81730fab08093
Jhony Taveira <jhony@jhony-debian.jhony.com>
2019-08-20 00:24:28 -0300

    Add banner with image profile

---
+++ app/javascript/components/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <section className={'banner-container'}>
+      <div className={'banner'}></div>
+      <div className={'container-photo'}>
+        <div className={'photo'}></div>
+      </div>
+    </section>
+  )
+}
+
+export default Banner
